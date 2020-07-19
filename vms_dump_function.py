f42d26ed08d5cef1fcb46e4c482936da46578cf9
Nikhil YS <nikhil.nyk007@gmail.com>
2020-07-19 22:18:28 +0530

    data

--- vms_dump_function.py
+++ vms_dump_function.py
@@ -6,10 +6,11 @@ pd.set_option('display.max_columns',None)
 pd.set_option('display.expand_frame_repr', False)
 pd.set_option('display.max_rows', None)
 
-vmsdump_df = pd.read_excel(r"E:\Nikhil\automation\invoicing\vms_dump.xlsx", header=0)
+vmsdump_df = pd.read_excel(r"E:\Nikhil\automation\Invoicing_automation\vms_dump.xlsx", header=0, sheet_name='Sheet2')
 vmsdump_df = vmsdump_df.set_index('RACF ID')    #Setting index to Racf id to make calculations easier
+vmsdump_df = vmsdump_df[['WeekEnding', 'Reg Hours', 'OT Hours']]  #Dropped the unused columns
 working_hrs_per_day = 8
-# print(vmsdump_df)
+print(vmsdump_df)
 
 # Function to read the excel in openpyxl and then transfer the data into a Pandas DataFrame
 def load_workbook_range(range_string, ws):
@@ -34,9 +35,28 @@ leave_tracker_df=leave_tracker_df.iloc[1:]
 leave_tracker_df.columns=header
 # print(leave_tracker_df)
 
+# ===================================================================================================================
+# Reads the user Input and then generates the calender days expected for the current month Invoicing.
+# This is needed as some teammates might take leave or submit 0 hours in the VMS and we don't get VMS data for those
+# To handle those cases we are first creating a perfect calender and then fill in details from VMS Dump.
+# In case any week is not available in the VMS Dump sheet, then that week would become 0.
+# ===================================================================================================================
+
+def create_default_calender(racf_id,start_date,end_date):
+    date_index=pd.date_range(start=start_date, end=end_date,freq='W')
+    vms_generated_calndr_df=pd.DataFrame(date_index, columns=['WeekEnding'])
+    vms_generated_calndr_df['RACF ID'] = racf_id
+    vms_generated_calndr_df = vms_generated_calndr_df.set_index('RACF ID')
+    # print (vms_generated_calndr_df)
+
+    return vms_generated_calndr_df
+
+
+
+
 # Reads the VMS Dump and create a pandas DataFrame with needed columns
-def generate_vms_sheet(racf_id):    #(leave_tracker_index,racf_id,start_date,end_date):
-    vmsdump_user_df = vmsdump_df.loc[racf_id, ['WeekEnding', 'Reg Hours', 'OT Hours']]  #Dropped the unused columns
+def generate_vms_sheet(racf_id,vms_generated_calndr_df):    #(leave_tracker_index,racf_id,start_date,end_date):
+    vmsdump_user_df = pd.merge(vms_generated_calndr_df,vmsdump_df,how='left',on=['WeekEnding'])
     vmsdump_user_df['vms_WeekStarting'] = vmsdump_user_df['WeekEnding'] + pd.offsets.Day(-6)
     vmsdump_user_df[['Reg Hours','OT Hours']] = vmsdump_user_df[['Reg Hours','OT Hours']].fillna(0) #Replace NaN with 0
     vmsdump_user_df['vms_pending_hours'] = vmsdump_user_df['Reg Hours'] + vmsdump_user_df['OT Hours']  #Created a new column for keeping VMS hours counter
@@ -46,9 +66,9 @@ def generate_vms_sheet(racf_id):    #(leave_tracker_index,racf_id,start_date,end
     # As the resample method doesn't expand the last entry till the end, so we have to add another duplicate last row for the same.
     vmsdump_user_df = vmsdump_user_df.append(vmsdump_user_df.iloc[-1])  #appends the last row again
     vmsdump_user_df.iloc[-1, vmsdump_user_df.columns.get_loc('vms_WeekStarting')] = vmsdump_user_df.iloc[-1, vmsdump_user_df.columns.get_loc('WeekEnding')]
-    vmsdump_user_df = vmsdump_user_df.reset_index().set_index('vms_WeekStarting').resample('D').ffill().reset_index().set_index('RACF ID')
+    # vmsdump_user_df = vmsdump_user_df.reset_index().set_index('vms_WeekStarting').resample('D').ffill().reset_index().set_index('RACF ID')
 
     print(vmsdump_user_df)
 
-
-generate_vms_sheet('UGAM211')
+# vms_generated_calndr_df=create_default_calender('UGAM211','2020-03-30','2020-04-26')
+# generate_vms_sheet('UINY1',vms_generated_calndr_df)
