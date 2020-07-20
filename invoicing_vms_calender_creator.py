29970ae4f87fb56de304fbdebe14f7b0cfe69caf
Nikhil YS <nikhil.nyk007@gmail.com>
2020-07-20 14:52:36 +0530

    bug fixes.

--- invoicing_vms_calender_creator.py
+++ invoicing_vms_calender_creator.py
@@ -22,12 +22,16 @@ df['vms_pending_hours'] = df['vms_hours'] - df.groupby('vms_week').final_output.
 
 df.loc[ (df['vms_working_days'] > df['leave_working_days']) & (df['weekend_flag'] == 1) & (df['leave_hours'] == 0) & (df['leave_working_wkenddays'] == 0), 'final_output' ] = 0
 df['vms_pending_hours'] = df['vms_hours'] - df.groupby('vms_week').final_output.transform('sum')
+
 # This is to handle if VMS days = Leave Tracker days
 # First set handles the weekdays and Second set handles the weekend.
 df.loc[(df['vms_working_days'] == df['leave_working_days']) &  (df['weekday_flag'] == 1) & (df['leave_hours'] > 0) & (df['leave_working_wkdays'] > 0), 'final_output'] = np.divide(a, b, out=np.zeros_like(a), where=b!=0)
 df['vms_pending_hours'] = df['vms_hours'] - df.groupby('vms_week').final_output.transform('sum')
 
 df.loc[(df['vms_working_days'] == df['leave_working_days']) &  (df['weekend_flag'] == 1) & (df['leave_hours'] > 0) & (df['leave_working_wkenddays'] > 0) & (df['vms_pending_hours'] > 0), 'final_output'] = np.divide(a, b, out=np.zeros_like(a), where=b!=0)
+df['vms_pending_hours'] = df['vms_hours'] - df.groupby('vms_week').final_output.transform('sum')
+
+df.loc[(df['vms_working_days'] == df['leave_working_days']) &  (df['weekend_flag'] == 1) & (df['leave_hours'] == 0) & (df['leave_working_wkenddays'] == 0) & (df['vms_pending_hours'] > 0), 'final_output'] = 0
 df['vms_pending_hours'] = df['vms_hours'] - df.groupby('vms_week').final_output.transform('sum')
 
 # This is to handle VMS < Leave Tracker days
@@ -50,7 +54,7 @@ def fill_missing(x):
     hours_counter = x['vms_pending_hours'].mean()
     working_hrs_per_day = 8
     # To check if VMS itself is filled as 0 hours but leave tracker had hours
-    if ( (hours_counter == 0) & (x['vms_leave_diff'].mean() != 0) ):
+    if ( (x['vms_working_days'].mean() == 0) & (x['vms_leave_diff'].mean() != 0) ):
         x['highlight_flag'] = 1
     else:
         x['highlight_flag'] = 0
@@ -72,5 +76,5 @@ df=df.groupby('vms_week').apply(fill_missing)
 # Below will make the vms_pending_hours equal to 0. Note running it, as it gives how much hours were not
 # calculated correctly
 # df['vms_pending_hours'] = df['vms_hours'] - df.groupby('vms_week').final_output.transform('sum')
-print (df[df['vms_week'] == '4/26'])
-# print(df)
+# print (df[df['vms_week'] == '4/12'])
+print(df)
