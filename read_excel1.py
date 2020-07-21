47716dd2f93648be9c5327f65524190e40bbb7f0
Nikhil YS <nikhil.nyk007@gmail.com>
2020-07-21 15:26:15 +0530

    updates

--- read_excel1.py
+++ read_excel1.py
@@ -17,12 +17,30 @@ def load_workbook_range(range_string, ws):
     # return pd.DataFrame(data_rows, columns=get_column_interval(col_start, col_end))
     return pd.DataFrame(data_rows)
 
-wb = load_workbook(filename=r'E:\Nikhil\automation\invoicing\Leave_Tracker_Marketing_Finance_2020.xlsx',
-                   read_only=True)
-ws = wb['Tracker']
-df=load_workbook_range('C17:JU37', ws)
-header=df.iloc[0]
-df=df.iloc[1:]
-df.columns=header
+# wb = load_workbook(filename=r'E:\Nikhil\automation\Invoicing_automation\Leave_Tracker_Marketing_Finance_2020.xlsx',
+                   # read_only=True)
+# ws = wb['Tracker']
+# df=load_workbook_range('C17:JU37', ws)
+# header=df.iloc[0]
+# df=df.iloc[1:]
+# df.columns=header
 
-print(df)
+start_index = 18
+end_index = 37
+
+def read_leave_tracker(start_index,end_index):
+    # print(start_index-1, end_index)
+    df=pd.read_excel(r'E:\Nikhil\automation\Invoicing_automation\Leave_Tracker_Marketing_Finance_2020.xlsx',
+    sheet_name='Tracker',skiprows=start_index-2, nrows=(end_index-start_index+1))
+    # df.columns = df.columns.str.strip()
+    df=df.set_index('RACF ID')
+    df=df.loc[:,'Resource Names ':]
+    df=df.reset_index()
+    del df['Resource Names ']
+    df=df.set_index('RACF ID')
+    df=df.T
+    df.index = pd.to_datetime(df.index)
+    return df
+
+print(read_leave_tracker(start_index,end_index))
+# print(df)
